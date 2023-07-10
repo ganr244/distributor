09f7c69a655f794e5b61526c3156a9ee1af5ccc7
r.ganeev <r.ganeev@bftcom.com>
2023-07-10 19:16:01 +0300

    m1l3. OOP

---
+++ m1l3-oop/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package ru.otus.otuskotlin.distributor.m1l3.oop
+
+fun main() {
+    println("M1L3 OOP!")
+}
---
+++ m1l3-oop/src/test/kotlin/OopTest.kt
@@ -0,0 +1,11 @@
+package ru.otus.otuskotlin.distributor.m1l3.oop
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class OopTest {
+    @Test
+    fun test(){
+        assertEquals(4, 2+2)
+    }
+}
