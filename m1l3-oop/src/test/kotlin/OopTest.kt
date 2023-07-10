ab632057de804c4d87bcc13824b881c467e1a5f9
r.ganeev <r.ganeev@bftcom.com>
2023-07-10 19:35:49 +0300

    m1l3. Interface + class

--- m1l3-oop/src/test/kotlin/OopTest.kt
+++ m1l3-oop/src/test/kotlin/OopTest.kt
@@ -1,11 +1,27 @@
 package ru.otus.otuskotlin.distributor.m1l3.oop
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import ru.otus.otuskotlin.distributor.m1l3.oop.FirstInterface as FirstInterface
+
+private interface FirstInterface {
+    val sv1: String
+    val sv2: String
+
+    fun voice1() {
+        println("sv1 = $sv1")
+    }
+
+    fun voice2() {
+        println("sv2 = $sv2")
+    }
+}
+
+private data class FirstClass(override val sv1: String = "1", override val sv2: String = "2"): FirstInterface
 
 class OopTest {
     @Test
     fun test(){
-        assertEquals(4, 2+2)
+        FirstClass().voice1()
+        FirstClass().voice2()
     }
 }
