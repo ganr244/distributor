7f382c973874992269ac028db43005e3969c2a76
r.ganeev <r.ganeev@bftcom.com>
2023-07-10 22:09:26 +0300

    m1l3. bugfix Otus home task

--- m1l3-oop/src/main/kotlin/figure/Figure.kt
+++ m1l3-oop/src/main/kotlin/figure/Figure.kt
@@ -6,4 +6,4 @@ interface Figure{
     fun area(): Int
 }
 
-fun diffArea(figure1: Figure, figure2: Figure): Int = (figure2.area() - figure1.area()).absoluteValue
+fun diffArea(figure1: Figure, figure2: Figure): Int = figure1.area() - figure2.area()
